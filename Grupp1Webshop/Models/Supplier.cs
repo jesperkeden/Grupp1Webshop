7bf8ee3b1d8a36f11f9296426f5e3cd5d09b8732
Extender <dann_137@hotmail.com>
2023-01-11 13:53:58 +0100

    Sup Ord

--- Grupp1Webshop/Models/Supplier.cs
+++ Grupp1Webshop/Models/Supplier.cs
@@ -8,5 +8,13 @@ namespace Grupp1Webshop.Models
 {
     internal class Supplier
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ContactPerson { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string StreetAdress { get; set; }
+        public string ZipCode { get; set; }
+        public string CityId { get; set; }
     }
 }
