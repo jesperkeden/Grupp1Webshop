191a0d92649785ffb989700fe41d046052ae843c
EminaDu <115992688+EminaDu@users.noreply.github.com>
2023-01-11 14:01:46 +0100

    user

--- Grupp1Webshop/Models/Basket.cs
+++ Grupp1Webshop/Models/Basket.cs
@@ -8,5 +8,6 @@ namespace Grupp1Webshop.Models
 {
     internal class Basket
     {
+
     }
 }
--- Grupp1Webshop/Models/User.cs
+++ Grupp1Webshop/Models/User.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace Grupp1Webshop.Models
 {
-    internal class Users
+    internal class User
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public int PhoneNumber { get; set; }
+        public bool Admin { get; set; }
+        public string StreetAdress { get; set; }
+        public int ZipCode { get; set; }
+        public int CityId { get; set; }
+
+
     }
 }
