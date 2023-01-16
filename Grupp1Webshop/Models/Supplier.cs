70dd5a3cab51b51ae8f3fd7fc5828314ef24d779
jesperkeden <83838381+jesperkeden@users.noreply.github.com>
2023-01-16 14:03:24 +0100

    string to int zipcode

--- Grupp1Webshop/Models/Supplier.cs
+++ Grupp1Webshop/Models/Supplier.cs
@@ -16,7 +16,7 @@ namespace Grupp1Webshop.Models
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
         public string StreetAdress { get; set; }
-        public string ZipCode { get; set; }
+        public int ZipCode { get; set; }
         [ForeignKey("City")]
         public int? CityId { get; set; }
         public City City { get; set; }
