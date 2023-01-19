ba53d84d44622d219fdc362499b0c2deed2eb0da
jesperkeden <83838381+jesperkeden@users.noreply.github.com>
2023-01-19 14:56:53 +0100

    Jesper order

--- Grupp1Webshop/Models/Order.cs
+++ Grupp1Webshop/Models/Order.cs
@@ -4,6 +4,8 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Grupp1Webshop.Data;
+using Grupp1Webshop.Models;
 
 namespace Grupp1Webshop.Models
 {
@@ -13,12 +15,30 @@ namespace Grupp1Webshop.Models
         {
             Product = new HashSet<Product>();
         }
+
         public int Id { get; set; }
         public string PaymenthMethod { get; set; }
         public string Shipping { get; set; }
         public double ShippingCost { get; set; }
         public double TotalCost { get; set; }
+        public bool HasPayed { get; set; } = false;
+        public Dictionary<int, Product> MyOrder { get; set; } 
         public User User { get; set; }
         public virtual ICollection<Product> Product { get; set; }
+
+        public void CreateMyOrder()
+        {
+            using var db = new Context();
+            var product = new Product();
+            Dictionary<int, Product> MyOrder = new Dictionary<int, Product>();
+        }
+
+
+        public void AddToMyOrder(Dictionary<int, Product> MyOrder, Product product)
+        {
+            using var db = new Context();
+            MyOrder.Add(User.Id, product);
+            db.SaveChanges();
+        }
     }
 }
